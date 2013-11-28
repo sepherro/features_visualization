45fbcebd93c14751526517eced699d90c092ea0f
Marek Kraft <marek.kraft@gmail.com>
2013-11-28 03:22:01 +0100

    Inlier count display is done, feature time measurement is done, needs updates for descriptor time, matching time and geometry accuracy. X axis scale is needed.

--- main.py
+++ main.py
@@ -4,169 +4,172 @@ import glob
 import os
 import numpy
 
-#for files in glob.glob("*.txt"):
-#    print(files)
+fast_seq1= []
+brisk_seq1= []
+gftt_seq1= []
+harris_seq1= []
+orb_seq1= []
+sift_seq1= []
+star_seq1= []
+surf_seq1= []
 
-inlier_ratio_brisk = numpy.zeros(6*14).reshape(6, 14)
-inlier_ratio_fast = numpy.zeros(6*14).reshape(6, 14)
-inlier_ratio_gftt = numpy.zeros(6*14).reshape(6, 14)
-inlier_ratio_harris = numpy.zeros(6*14).reshape(6, 14)
-inlier_ratio_orb = numpy.zeros(6*14).reshape(6, 14)
-inlier_ratio_sift = numpy.zeros(6*14).reshape(6, 14)
-inlier_ratio_star = numpy.zeros(6*14).reshape(6, 14)
-inlier_ratio_surf = numpy.zeros(6*14).reshape(6, 14)
+fast_seq_mean = []
+brisk_seq_mean =[]
+gftt_seq_mean = []
+harris_seq_mean = []
+orb_seq_mean = []
+sift_seq_mean = []
+star_seq_mean = []
+surf_seq_mean = []
+
+seq_temp = numpy.zeros(114)
+
+
+def plot_set(plot_data):
+    legend_labels = ("BRIEF", "BRISK", "FREAK", "ORB", "SIFT", "SURF")
+    for i in range (0, 6):
+        plot(plot_data[i][0:45], label=legend_labels[i])           # tu wystarczy zmienic przedzial, zeby narysowac odpowiednia czesc wykresu
+
+
+
+
+def get_processing_time(dataset_path):
+    file_data = genfromtxt(dataset_path, delimiter=';')
+    stripped_name = dataset_path.split("\\")[1]
+    detector = stripped_name.split("_")[0]
+    descriptor =stripped_name.split("_")[1]
+
+    #do matchingu
+    #27 matching time # 28 calculation time # 2 nMatches
+
+    time_per_descriptor = []
+    time_per_match = []
+    time_per_descriptor =  file_data[1:115,3]/file_data[1:115, 2] #TODO: zapytac Adama co znacza czasy
+    time_per_match = ( file_data[1:115,27]/(file_data[1:115,27]) ) / file_data[1:115,27]
+
+    return(detector, descriptor, time_per_descriptor, time_per_match)
+
+
+
 
 for line in glob.glob("results_set001\*.txt"):
     for word in line.split():
         if line.endswith("_stat.txt")==False:
-            my_data = genfromtxt(line, delimiter=';')
-            line = line.replace('results_set001\\', '')
-            print(line)
-            detector = line.split("_")[0]
-            descriptor = line.split("_")[1]
-
-            if detector == "brisk":
-                if descriptor == "brief":
-                    inlier_ratio_brisk[0, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
-                if descriptor == "brisk":
-                    inlier_ratio_brisk[1, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
-                if descriptor == "freak":
-                    inlier_ratio_brisk[2, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
-                if descriptor == "orb":
-                    inlier_ratio_brisk[3, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
-                if descriptor == "sift":
-                    inlier_ratio_brisk[4, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
-                if descriptor == "surf":
-                    inlier_ratio_brisk[5, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
-
-            if detector == "fast":
-                if descriptor == "brief":
-                    inlier_ratio_fast[0, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
-                if descriptor == "brisk":
-                    inlier_ratio_fast[1, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
-                if descriptor == "freak":
-                    inlier_ratio_fast[2, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
-                if descriptor == "orb":
-                    inlier_ratio_fast[3, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
-                if descriptor == "sift":
-                    inlier_ratio_fast[4, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
-                if descriptor == "surf":
-                    inlier_ratio_fast[5, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
-
-            if detector == "gftt":
-                if descriptor == "brief":
-                    inlier_ratio_gftt[0, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
-                if descriptor == "brisk":
-                    inlier_ratio_gftt[1, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
-                if descriptor == "freak":
-                    inlier_ratio_gftt[2, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
-                if descriptor == "orb":
-                    inlier_ratio_gftt[3, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
-                if descriptor == "sift":
-                    inlier_ratio_gftt[4, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
-                if descriptor == "surf":
-                    inlier_ratio_gftt[5, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
-
-            if detector == "harris":
-                if descriptor == "brief":
-                    inlier_ratio_harris[0, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
-                if descriptor == "brisk":
-                    inlier_ratio_harris[1, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
-                if descriptor == "freak":
-                    inlier_ratio_harris[2, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
-                if descriptor == "orb":
-                    inlier_ratio_harris[3, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
-                if descriptor == "sift":
-                    inlier_ratio_harris[4, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
-                if descriptor == "surf":
-                    inlier_ratio_harris[5, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
-
-            if detector == "orb":
-                if descriptor == "brief":
-                    inlier_ratio_orb[0, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
-                if descriptor == "brisk":
-                    inlier_ratio_orb[1, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
-                if descriptor == "freak":
-                    inlier_ratio_orb[2, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
-                if descriptor == "orb":
-                    inlier_ratio_orb[3, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
-                if descriptor == "sift":
-                    inlier_ratio_orb[4, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
-                if descriptor == "surf":
-                    inlier_ratio_orb[5, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
-
-            if detector == "sift":
-                if descriptor == "brief":
-                    inlier_ratio_sift[0, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
-                if descriptor == "brisk":
-                    inlier_ratio_sift[1, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
-                if descriptor == "freak":
-                    inlier_ratio_sift[2, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
-                if descriptor == "orb":
-                    inlier_ratio_sift[3, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
-                if descriptor == "sift":
-                    inlier_ratio_sift[4, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
-                if descriptor == "surf":
-                    inlier_ratio_sift[5, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
-
-            if detector == "star":
-                if descriptor == "brief":
-                    inlier_ratio_star[0, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
-                if descriptor == "brisk":
-                    inlier_ratio_star[1, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
-                if descriptor == "freak":
-                    inlier_ratio_star[2, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
-                if descriptor == "orb":
-                    inlier_ratio_star[3, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
-                if descriptor == "sift":
-                    inlier_ratio_star[4, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
-                if descriptor == "surf":
-                    inlier_ratio_star[5, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
+            (det, desc, inl) = get_inlier_percentage(line)
+            if det == "brisk":
+                brisk_seq1.append(inl)
+                print(desc)
+            if det == "fast":
+                fast_seq1.append(inl)
+            if det == "gftt":
+                gftt_seq1.append(inl)
+            if det == "harris":
+                harris_seq1.append(inl)
+            if det == "orb":
+                orb_seq1.append(inl)
+            if det == "sift":
+                sift_seq1.append(inl)
+            if det == "star":
+                star_seq1.append(inl)
+            if det == "surf":
+                surf_seq1.append(inl)
 
 
-            if detector == "surf":
-                if descriptor == "brief":
-                    inlier_ratio_surf[0, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
-                if descriptor == "brisk":
-                    inlier_ratio_surf[1, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
-                if descriptor == "freak":
-                    inlier_ratio_surf[2, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
-                if descriptor == "orb":
-                    inlier_ratio_surf[3, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
-                if descriptor == "sift":
-                    inlier_ratio_surf[4, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
-                if descriptor == "surf":
-                    inlier_ratio_surf[5, 0:14] =  my_data[50:64,3]/my_data[50:64, 2]
 
-legend_labels = ("BRIEF", "BRISK", "FREAK", "ORB", "SIFT", "SURF")
+for i in range(0, 6):
+    seq_temp = (brisk_seq1[0+i] + brisk_seq1[6+i] + brisk_seq1[12+i] + brisk_seq1[18+i] + brisk_seq1[24+i] + brisk_seq1[30+i])/6
+    brisk_seq_mean.append(seq_temp)
+
+for i in range(0, 6):
+    seq_temp = (fast_seq1[0+i] + fast_seq1[6+i] + fast_seq1[12+i] + fast_seq1[18+i] + fast_seq1[24+i] + fast_seq1[30+i])/6
+    fast_seq_mean.append(seq_temp)
+
+for i in range(0, 6):
+    seq_temp = (gftt_seq1[0+i] + gftt_seq1[6+i] + gftt_seq1[12+i] + gftt_seq1[18+i] + gftt_seq1[24+i] + gftt_seq1[30+i])/6
+    gftt_seq_mean.append(seq_temp)
+
+for i in range(0, 6):
+    seq_temp = (harris_seq1[0+i] + harris_seq1[6+i] + harris_seq1[12+i] + harris_seq1[18+i] + harris_seq1[24+i] + harris_seq1[30+i])/6
+    harris_seq_mean.append(seq_temp)
+
+for i in range(0, 6):
+    seq_temp = (orb_seq1[0+i] + orb_seq1[6+i] + orb_seq1[12+i] + orb_seq1[18+i] + orb_seq1[24+i] + orb_seq1[30+i])/6
+    orb_seq_mean.append(seq_temp)
+
+##################### special treatment - no SIFT-ORB, inserting dummy data
+
+#idx = ( 3, 9, 15, 21, 27, 33)
+#np.insert(sift_seq1, idx, 0, axis=1)
+
+sift_seq1.insert( 3, np.zeros(116))
+sift_seq1.insert( 9, np.zeros(116))
+sift_seq1.insert( 15, np.zeros(116))
+sift_seq1.insert( 21, np.zeros(116))
+sift_seq1.insert( 27, np.zeros(116))
+sift_seq1.insert( 33, np.zeros(116))
+#print(sift_seq1)
+for i in range(0, 6):
+    seq_temp = (sift_seq1[0+i] + sift_seq1[6+i] + sift_seq1[12+i] + sift_seq1[18+i] + sift_seq1[24+i] + sift_seq1[30+i])/6
+    sift_seq_mean.append(seq_temp)
 
 
-for i in range (0, 5):
-    plot(inlier_ratio_fast[i], label = legend_labels[i])
-    print(legend_labels[i])
+##################
 
-legend(loc='upper right')
-plt.figure()
+for i in range(0, 6):
+    seq_temp = (star_seq1[0+i] + star_seq1[6+i] + star_seq1[12+i] + star_seq1[18+i] + star_seq1[24+i] + star_seq1[30+i])/6
+    star_seq_mean.append(seq_temp)
+    
+for i in range(0, 6):
+    seq_temp = (surf_seq1[0+i] + surf_seq1[6+i] + surf_seq1[12+i] + surf_seq1[18+i] + surf_seq1[24+i] + surf_seq1[30+i])/6
+    surf_seq_mean.append(seq_temp)
+    
 
-for i in range (0, 5):
-    plot(inlier_ratio_sift[i], label = legend_labels[i])
-    print(legend_labels[i])
 
-legend(loc='upper right')
-plt.figure()
 
-for i in range (0, 5):
-    plot(inlier_ratio_star[i], label = legend_labels[i])
-    print(legend_labels[i])
+plt.subplot(2, 4, 1)
+plt.ylim([0, 1])
+plot_set(brisk_seq_mean)
+plt.title("BRISK")
 
-legend(loc='upper right')
-plt.figure()
+plt.subplot(2, 4, 2)
+plt.ylim([0, 1])
+plot_set(fast_seq_mean)
+plt.title("FAST")
 
-for i in range (0, 5):
-    plot(inlier_ratio_orb[i], label = legend_labels[i])
-    print(legend_labels[i])
+plt.subplot(2, 4, 3)
+plt.ylim([0, 1])
+plot_set(gftt_seq_mean)
+plt.title("GFTT")
 
-legend(loc='upper right')
+plt.subplot(2, 4, 4)
+plt.ylim([0, 1])
+plot_set(harris_seq_mean)
+plt.title("HARRIS")
+
+plt.subplot(2, 4, 5)
+plt.ylim([0, 1])
+plot_set(orb_seq1)
+plt.title("ORB")
+
+plt.subplot(2, 4, 6)
+plt.ylim([0, 1])
+plot_set(sift_seq_mean)
+plt.title("SIFT")
+
+plt.subplot(2, 4, 7)
+plt.ylim([0, 1])
+plot_set(star_seq_mean)
+plt.title("STAR")
+
+plt.subplot(2, 4, 8)
+plt.ylim([0, 1])
+plot_set(surf_seq_mean)
+plt.title("SURF")
+
+legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
 
 show()
+
+
+
 
---
+++ main_times.py
@@ -0,0 +1,45 @@
+from numpy import genfromtxt
+from pylab import *
+import glob
+import os
+import numpy
+
+
+def get_processing_time(dataset_path):
+    file_data = genfromtxt(dataset_path, delimiter=';')
+    stripped_name = dataset_path.split("\\")[1]
+    detector = stripped_name.split("_")[0]
+    descriptor =stripped_name.split("_")[1]
+
+    #do cech
+    # 1 nFeatures # 2 processing time
+
+    time_per_feature = []
+    time_per_feature =  (file_data[1:115,2]*1000)/file_data[1:115, 1]
+    avg_time = sum(time_per_feature)/len(time_per_feature)
+
+    print("time per feature is ", avg_time, "milliseconds")
+
+    return(detector, avg_time)
+
+proc_times = []
+labels = ("BRISK", "FAST", "GFTT", "HARRIS", "ORB", "SIFT", "STAR", "SURF")
+
+proc_times.append( get_processing_time("results_set001\\brisk_brisk_set001_stat.txt")[1] )
+proc_times.append( get_processing_time("results_set001\\fast_brisk_set001_stat.txt")[1] )
+proc_times.append( get_processing_time("results_set001\\gftt_brisk_set001_stat.txt")[1] )
+proc_times.append( get_processing_time("results_set001\\harris_brisk_set001_stat.txt")[1] )
+proc_times.append( get_processing_time("results_set001\\orb_brisk_set001_stat.txt")[1] )
+proc_times.append( get_processing_time("results_set001\\sift_brisk_set001_stat.txt")[1] )
+proc_times.append( get_processing_time("results_set001\\star_brisk_set001_stat.txt")[1] )
+proc_times.append( get_processing_time("results_set001\\surf_brisk_set001_stat.txt")[1] )
+
+#TODO: Dobrać odpowiednio czasy
+
+positions = (1, 2, 3, 4, 5, 6, 7, 8)
+
+plt.bar(positions, proc_times, align='center')
+plt.xticks(positions, labels)
+plt.show()
+
+
